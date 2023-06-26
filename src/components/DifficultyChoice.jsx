555f5d92f89af80260f2c7d1ca7125f3fa7cfbda
LorenzCastillo <lorenz5524@hotmail.com>
2023-06-26 02:27:38 -0700

    Changed the get request to an axios request

--- src/components/DifficultyChoice.jsx
+++ src/components/DifficultyChoice.jsx
@@ -16,7 +16,7 @@ const DifficultyChoice = (props) => {
 
 	return (
 		<>
-			<Link to={`/${quizLink}/${props.choice}/quiz`}>
+			<Link to={`/${quizLink}/${props.value}/quiz`}>
 				<button onClick={handleQuizContext}>
 					<div className="flex w-96 h-20 bg-custom-red text-white rounded-xl items-center justify-center">
 						<h1 className="font-alte-bold text-3xl">{props.choice}</h1>
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import QuizOption from "../components/QuizOption";
 import QuizContext from "../context/QuizContext";
+import axios from "axios";
 
 const QuizPage = () => {
 
@@ -15,15 +16,14 @@ const QuizPage = () => {
 	}, []);
 
 	const fetchData = async () => {
-		try {
-			const response = await fetch(`https://opentdb.com/api.php?amount=${quizType[1]}&category=${quizType[0]}&difficulty=${quizType[2]}&type=multiple`);
-			const responseData = await response.json();
-			setQuizData(responseData);
-			setLoading(false);
-			console.log(responseData);
-		} catch (error) {
-			console.log(error);
-		}
+		axios.get(`https://opentdb.com/api.php?amount=${quizType[1]}&category=${quizType[0]}&difficulty=${quizType[2]}&type=multiple`)
+			.then(response => {
+				setQuizData(response.data);
+				setLoading(false);
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	};
 
 	return loading ? (
@@ -31,7 +31,7 @@ const QuizPage = () => {
 			<Navbar/>
 			<div className="flex flex-col w-full my-10 items-center">
 				<div className="w-[28rem] h-32">
-					<h1 className="font-alte-bold text-2xl text-white text-center">Loading...</h1>
+					<h1 className="font-alte-bold text-4xl text-white text-center">Loading...</h1>
 				</div>
 			</div>
 		</>
